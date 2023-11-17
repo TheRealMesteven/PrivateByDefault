be0df4a9905b145c01a63c72d9d9fa4709ce4311
Mest <mestevenemail@gmail.com>
2023-11-17 08:11:21 +0000

    Add project files.

---
+++ PrivateByDefault/Mod.cs
@@ -0,0 +1,31 @@
+﻿using PulsarModLoader;
+using PulsarModLoader.CustomGUI;
+using UnityEngine;
+
+namespace PrivateByDefault
+{
+    public class Mod : PulsarMod
+    {
+        public override string HarmonyIdentifier() => "Mest.PrivateByDefault";
+        public override string Name => "Private By Default";
+        public override string Version => "0.0.1";
+        public override string Author => "Mest";
+        public override string ShortDescription => "When creating a game, default setting is private.";
+    }
+    internal class Config : ModSettingsMenu
+    {
+        public override void Draw()
+        {
+            GUILayout.BeginHorizontal();
+            Enabled.Value = GUILayout.Toggle(Enabled.Value, "Private games by default");
+            PasswordEnabled.Value = GUILayout.Toggle(PasswordEnabled.Value, "Use last password");
+            GUILayout.Label($"Last Password: {Password.Value}");
+            GUILayout.EndHorizontal();
+        }
+
+        public override string Name() => "Private By Default Config";
+        public static SaveValue<bool> Enabled = new SaveValue<bool>("Enabled", true);
+        public static SaveValue<bool> PasswordEnabled = new SaveValue<bool>("PasswordEnabled", true);
+        public static SaveValue<string> Password = new SaveValue<string>("Password", "");
+    }
+}
---
+++ PrivateByDefault/Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace PrivateByDefault
+{
+    [HarmonyPatch(typeof(PLUICreateGameMenu), "Enter")]
+    internal class CreateGamePatch
+    {
+        private static void Postfix(PLUICreateGameMenu __instance)
+        {
+            __instance.IsPrivateGame.isOn = Config.Enabled.Value;
+            if (Config.PasswordEnabled.Value) __instance.PasswordField.text = Config.Password.Value;
+        }
+    }
+    [HarmonyPatch(typeof(PLUICreateGameMenu), "ClickEngage")]
+    internal class EngagePatch
+    {
+        private static void Prefix(PLUICreateGameMenu __instance)
+        {
+            Config.Password.Value = __instance.PasswordField.text;
+        }
+    }
+}
